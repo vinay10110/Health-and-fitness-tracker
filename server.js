d0f77a4c06115eb0fbcbba659b79930f118f33bd
vinay10110 <vinaychakravarthi10110@gmail.com>
2024-12-16 13:07:17 +0530

    make changes in server to deploy the application

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
 const express = require('express');
-const app = express();
-const mongoose=require('mongoose');
-const cors=require('cors')
-app.use(express.json());
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
-const authRoute=require('./routes/auth');
-const healthRoute=require('./routes/healthTracker');
 
+const app = express();
+
+app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: `${process.env.HOST_ADDRESS}`,
   methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With'],
-  credentials:true
+  credentials: true
 }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  next();
-})
-app.use('/auth',authRoute);
-app.use('/health',healthRoute);
-mongoose.connect(
-    process.env.MONGO_URL 
-  );
-const PORT = 4000; 
+
+
+const authRoute = require('./routes/auth');
+const healthRoute = require('./routes/healthTracker');
+
+app.use('/auth', authRoute);
+app.use('/health', healthRoute);
+
+
+app.use(express.static(path.join(__dirname, "client", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Express server running at http://localhost:${PORT}`);
 });
